b16a41070c9567d1eaf1b478aa3b73fccd6095ea
x913 <x913x913@gmail.com>
2021-04-14 19:34:22 +0500

    firebase authorization and registration

--- app/src/main/java/com/k3/discordremake/ui/login/LoginActivity.kt
+++ app/src/main/java/com/k3/discordremake/ui/login/LoginActivity.kt
@@ -2,25 +2,109 @@ package com.k3.discordremake.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.k3.discordremake.R
 import com.k3.discordremake.databinding.ActivityMainBinding
 import com.k3.discordremake.ui.channels.ChannelsActivity
+import com.k3.discordremake.ui.signup.SignUpActivity
+import org.jetbrains.anko.design.longSnackbar
 import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.longToast
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@LoginActivity, R.layout.activity_main)
-        setupLogin()
+        if(auth.currentUser != null) {
+            startActivity(intentFor<ChannelsActivity>())
+        } else {
+            setupLogin()
+            setupSignUp()
+            setupTextFields()
+            setupForgotPassword()
+        }
     }
 
     private fun setupLogin() {
         binding.signInButton.setOnClickListener {
-            startActivity(intentFor<ChannelsActivity>())
+
+            when {
+                !isEmailValid(binding.emailTextView.text.toString()) -> {
+                    binding.emailSignUpTextInputLayout.error = "Email is not valid"
+                }
+                !isPasswordValid(binding.passwordTextView.text.toString()) -> {
+                    binding.emailPasswordTextInputLayout.error = "Password is not valid"
+                }
+                else -> {
+                    binding.loadingProgress.visibility = View.VISIBLE
+                    auth.signInWithEmailAndPassword(binding.emailTextView.text.toString(), binding.passwordTextView.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful && it.isComplete) {
+                            startActivity(intentFor<ChannelsActivity>())
+                            longToast("Welcome ${auth.currentUser?.displayName}")
+                        } else {
+                            binding.loadingProgress.visibility = View.GONE
+                            binding.container.longSnackbar(it.exception?.localizedMessage.toString())
+                        }
+                    }
+                }
+            }
         }
     }
+
+    private fun setupForgotPassword() {
+        binding.forgotTextView.setOnClickListener {
+            forgotPassword(it)
+        }
+    }
+
+    private fun setupSignUp() {
+        binding.signUpButton.setOnClickListener {
+            startSignUpActivity(it)
+        }
+    }
+
+    private fun startSignUpActivity(view: View) {
+        startActivity(intentFor<SignUpActivity>())
+    }
+
+    private fun forgotPassword(view: View) {
+        if(!isEmailValid(binding.emailTextView.text.toString())) {
+            binding.emailSignUpTextInputLayout.error = getString(R.string.email_not_valid)
+        } else {
+            auth.sendPasswordResetEmail(binding.emailTextView.text.toString()).addOnCompleteListener {
+                if(it.isComplete && it.isSuccessful) {
+                    binding.container.longSnackbar(getString(R.string.recover_email_sent))
+                } else {
+                    binding.container.longSnackbar(it.exception?.message.toString())
+                }
+            }
+        }
+    }
+
+    private fun setupTextFields() {
+        binding.emailTextView.doAfterTextChanged {
+            if(it != null) {
+                binding.emailTextView.error = null
+            }
+        }
+
+        binding.passwordTextView.doAfterTextChanged {
+            if(isPasswordValid(it.toString())) {
+                binding.passwordTextView.error = null
+            }
+        }
+
+    }
+
+    private fun isEmailValid(email: String): Boolean = email.contains('@') && email.isNotBlank()
+
+    private fun isPasswordValid(password: String): Boolean = password.length >= 6
+
 }
--- app/src/main/java/com/k3/discordremake/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/k3/discordremake/ui/signup/SignUpActivity.kt
@@ -1,15 +1,79 @@
 package com.k3.discordremake.ui.signup
 
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.k3.discordremake.R
+import com.k3.discordremake.databinding.ActivitySignUpBinding
+import com.k3.discordremake.ui.channels.ChannelsActivity
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.intentFor
 
 class SignUpActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivitySignUpBinding
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this@SignUpActivity, R.layout.activity_sign_up)
+        setupSignUp()
     }
+
+    private fun setupSignUp() {
+        binding.signUpButton.setOnClickListener {
+            when {
+                !isEmailValid(binding.emailTextInput.text.toString()) -> {
+                    binding.emailSignUpTextInputLayout.error = getString(R.string.email_not_valid)
+                }
+
+                !isPasswordValid(binding.firstPasswordText.text.toString(), binding.secondPasswordText.text.toString()) -> {
+                    binding.firstPasswordSignUpTextInputLayout.error = getString(R.string.password_not_valid_for_registration)
+                }
+
+                !isUserNameValid(binding.userNameTextInput.text.toString()) -> {
+                    binding.userNameTextInputLayout.error = getString(R.string.user_name_not_valid)
+                }
+
+                else -> {
+                    // sign up user
+                    binding.signUpProgress.visibility = View.VISIBLE
+                    auth.createUserWithEmailAndPassword(binding.emailTextInput.text.toString(), binding.firstPasswordText.text.toString()).addOnCompleteListener {
+                        binding.signUpProgress.visibility = View.GONE
+                        if(it.isSuccessful && it.isComplete) {
+                            val profileRequestUpdate = UserProfileChangeRequest.Builder()
+                                .setDisplayName(binding.userNameTextInput.text.toString())
+                                .build()
+
+                            auth.currentUser?.updateProfile(profileRequestUpdate)
+                            startActivity(intentFor<ChannelsActivity>())
+
+                        } else {
+                            binding.container.longSnackbar(it.exception?.localizedMessage.toString())
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    private fun isUserNameValid(userName: String): Boolean = userName.isNotBlank()
+
+    private fun isEmailValid(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    private fun isPasswordValid(password: String, secondaryPassword: String): Boolean =
+        password.isNotBlank() &&
+                secondaryPassword.isNotBlank() &&
+                password == secondaryPassword &&
+                password.length >= 6
+
 }
