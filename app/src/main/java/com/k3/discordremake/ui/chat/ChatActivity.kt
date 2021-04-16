632bde8f4a2a83c8a0ff89952c0089b786c5a32d
x913 <x913x913@gmail.com>
2021-04-16 16:18:38 +0500

    implement messaging

--- app/src/main/java/com/k3/discordremake/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/k3/discordremake/ui/chat/ChatActivity.kt
@@ -7,18 +7,23 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
 import com.k3.discordremake.R
+import com.k3.discordremake.data.model.Message
 import com.k3.discordremake.databinding.ActivityChatBinding
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.design.longSnackbar
 import java.lang.IllegalArgumentException
 
-class ChatActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity(), AnkoLogger {
 
     private lateinit var binding: ActivityChatBinding
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var firestore: FirebaseFirestore
     private lateinit var channelRef: DocumentReference
     private lateinit var messagesQuery: Query
@@ -31,8 +36,14 @@ class ChatActivity : AppCompatActivity() {
         title = intent.extras!!.getString(KEY_CHANNEL_NAME)
         initFirestore(channelId)
         initRecyclerView()
+        setupListeners()
     }
 
+    private fun setupListeners() {
+        binding.sendButton.setOnClickListener {
+            sendClicked()
+        }
+    }
 
     private fun initFirestore(channelId: String) {
         firestore = FirebaseFirestore.getInstance()
@@ -47,14 +58,12 @@ class ChatActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        Log.d("AAA", "Creating Chat Adapter")
         adapter = object : ChatAdapter(messagesQuery) {
             override fun onError(e: FirebaseFirestoreException) {
                 binding.containerLayout.longSnackbar(e?.message.toString())
             }
 
             override fun onDataChanged() {
-                Log.d("AAA", "onDataChanged")
                 if(itemCount == 0) {
                     binding.chatEmptyView.visibility = View.VISIBLE
                     binding.messagesRecyclerView.visibility = View.GONE
@@ -80,8 +89,29 @@ class ChatActivity : AppCompatActivity() {
         adapter.stopListening()
     }
 
-    fun sendClicked(view: View) {
+    private fun addMessage(text: String): Task<Void> {
+        val chatReference = channelRef
+            .collection(MESSAGE_COLLECTION)
+            .document()
 
+        val currentUser = auth.currentUser
+        // we don`t need timestamp because it's automatically taken from annotation
+        val message = Message(text, currentUser?.uid ?: "", currentUser.displayName, null)
+        return firestore.runTransaction {
+            it[chatReference] = message
+            null
+        }
+    }
+
+    fun sendClicked() {
+        addMessage(binding.messageTextField.text.toString())
+            .addOnSuccessListener(this) {
+                // reset
+                binding.messageTextField.setText("")
+            }
+            .addOnFailureListener(this) {
+                binding.containerLayout.longSnackbar(getString(R.string.network_error))
+            }
     }
 
     companion object {
